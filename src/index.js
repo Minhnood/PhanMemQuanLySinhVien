d572bb60e8b881f7b66d634a27fe0bbd6402f6ec
trieuminh <huynhtrieuminh6@gmail.com>
2025-03-17 18:58:17 +0700

    minh

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './store';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter basename="/PhanMemQuanLySinhVien">
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 );
